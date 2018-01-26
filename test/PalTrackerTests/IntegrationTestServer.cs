d3639954d8a915ba83796cf1fbb8969df6bf1783
Alex Sartan <asartan@pivotal.io>
2018-01-26 10:30:42 -0700

    Add tests for externalized configuration lab

---
+++ test/PalTrackerTests/IntegrationTestServer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using PalTracker;
+
+namespace PalTrackerTests
+{
+    public static class IntegrationTestServer
+    {
+        private static string ContentRoot =>
+            Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "..", "src", "PalTracker"));
+
+        public static TestServer Start() => new TestServer(
+            new WebHostBuilder()
+                .UseContentRoot(ContentRoot)
+                .ConfigureAppConfiguration((hostContext, config) =>
+                {
+                    var env = hostContext.HostingEnvironment;
+                    config.Sources.Clear();
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                    config.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+                    config.AddEnvironmentVariables();
+                })
+                .UseStartup<Startup>()
+        );
+    }
+}
